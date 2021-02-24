8ae82128b5b30932aee03c02a421e84d5c5f8e9a
Alison McGuinness <alison.mcguinness@gmail.com>
2021-02-24 21:37:25 +0000

    filtering panda dataset

--- mod1_20_numpy.py
+++ mod1_20_numpy.py
@@ -25,3 +25,14 @@ print(np_weight_lb[50])
 # Print out sub-array of np_height_in: index 100 up to and including index 110
 print(np_height_in[100:111])
 
+
+# Create new arrays for BOOLEAN stuff
+
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than 18.5 or smaller than 10
+print(np.logical_or(my_house > 18.5, my_house < 10))
+
+# Both my_house and your_house smaller than 11
+print(np.logical_and(my_house < 11, your_house < 11))
--- mod2_20_pandadataframe.py
+++ mod2_20_pandadataframe.py
@@ -1,5 +1,6 @@
 # Import cars data
 import pandas as pd
+import numpy as np
 dir = 'C:\\Users\\Admin\\PycharmProjects\\UCDExercises'
 file = ''
 cars = pd.read_csv(dir + '\\cars.csv', index_col=0)
@@ -42,3 +43,50 @@ print('each index must be a slice?? ')
 print(cars.iloc[2:4,0:1])
 print('or can pass in list of non consec indexes')
 print(cars.iloc[[2,4],[0,1]])
+
+print('4. to get ALL a column use : as first paramter to get all rows')
+# Print out drives_right column as Series
+print('column as a series')
+print(cars.loc[:, 'drives_right'])
+
+
+# Print out drives_right column as DataFrame
+print('column as data frame')
+print(cars.loc[:,['drives_right']])
+
+print('multi cols as data frame')
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:,['drives_right', 'cars_per_cap']])
+
+
+print('FILTERING DATA in PANDAS dataframe')
+print(' can pass in a list of booleans to pick out only rows where corresponding boolean is True')
+print(' (i) select the column  eg cars[''drives_right'']')
+print(' (ii) add the comparison eg cars[''drives_right''] == True - this gives series of booleans')
+print(' (iii) use that to index the area to only get the rows with ''True''')
+print('  (iv) put all above into one line! eg cars[cars[''drives_right''] == True')
+
+print('eg get all right hand drive ')
+
+# Extract drives_right column as Series: dr
+dr = cars['drives_right']
+print(dr)
+# Use dr to subset cars: sel
+sel = cars[dr]
+# Print sel
+print(sel)
+print('or one liner')
+print(cars[cars['drives_right']])   # this works becaus the column is already a boolean# so don't need the compare step!
+
+print('check all rows where one col passes filter')
+# Create car_maniac: observations that have a cars_per_cap over 500
+car_maniac = cars['cars_per_cap'] > 500
+# Print car_maniac
+print(car_maniac)
+print(cars[car_maniac])
+
+print('multiple filters use the numpy logical_and etc methods to concatenate')
+# Create medium: observations with cars_per_cap between 100 and 500
+medium = cars[np.logical_and(cars['cars_per_cap'] >=100, cars['cars_per_cap'] <=500)]
+# Print medium
+print(medium)
